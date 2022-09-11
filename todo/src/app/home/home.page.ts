42c81683fafb14848b77989bb3f51e883810cb94
NDOH NDOH Jules Moise Yannick <ndohmoise@gmail.com>
2022-09-11 18:56:16 +0200

    Mise à jour de la page d'accueil

--- todo/src/app/home/home.page.ts
+++ todo/src/app/home/home.page.ts
@@ -5,7 +5,28 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+
+  todoList = [{
+    itemName: 'Coding',
+    itemDueDate: '13-10-21',
+    itemPriority: 'high',
+    itemCategory: 'Work'
+  },
+  {
+    itemName: 'Design',
+    itemDueDate: '03-10-21',
+    itemPriority: 'low',
+    itemCategory: 'Work'
+  },
+  {
+    itemName: 'Shopping',
+    itemDueDate: '13-10-21',
+    itemPriority: 'middle',
+    itemCategory: 'Personnel'
+  }
+];
 
   constructor() {}
 
